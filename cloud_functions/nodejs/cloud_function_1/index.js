7df41353dbae6de5fa9e0e6f089ddf27bc2c6606
Alan <alan.abraham.tm@gmail.com>
2024-08-28 18:12:27 +0100

    add files

---
+++ cloud_functions/nodejs/cloud_function_1/index.js
@@ -0,0 +1,17 @@
+const functions = require('@google-cloud/functions-framework');
+const { Firestore } = require('@google-cloud/firestore');
+
+const firestore = new Firestore();
+
+functions.http('cloudFunction1', async (req, res) => {
+    console.log(`Starting cloudFunction1.`);
+
+    // get the region record
+    const regionRecord = await firestore.collection('regions').where('map_level', '==', 'Level1').get();
+
+    if (regionRecord.empty) {
+        res.status(404).send({ message: 'No records found.'});
+    }
+
+    res.status(200).send({ message: 'Found the record.'});
+});
