0a67fe37e3693ba85350e0967c9d075631860faa
Alan <alan.abraham.tm@gmail.com>
2024-08-31 18:41:34 +0100

    more changes today

--- cloud_functions/cloud_function_1/index.js
+++ cloud_functions/cloud_function_1/index.js
@@ -4,14 +4,14 @@ const { Firestore } = require('@google-cloud/firestore');
 const firestore = new Firestore();
 
 functions.http('cloud-function-1', async (req, res) => {
-    console.log(`Starting cloudFunction2l .`);
+    console.log(`Starting cloudFunction2.`);
 
     // get the region record
-    const regionRecord = await firestore.collection('regions').where('map_level', '==', 'Level1').get();
+    // const regionRecord = await firestore.collection('regions').where('map_level', '==', 'Level1').get();
 
-    if (regionRecord.empty) {
-        res.status(404).send({ message: 'No records found.'});
-    }
+    // if (regionRecord.empty) {
+    //     res.status(404).send({ message: 'No records found.'});
+    // }
 
     res.status(200).send({ message: 'Found the record.'});
 });
